d1c70ec41e922a2c469ec5a1db9489e3cf958aa2
tyland5 <tyland5@users.noreply.github.com>
2022-06-23 15:14:05 -0400

    added media query to js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 
 const sections = document.querySelectorAll("section[id]");
 
-window.addEventListener("scroll", navHighlighter);
+const screenSize = window.matchMedia('(min-width: 1024px)')
+
+screenSize.addEventListener("change", updateNavFunction)
+updateNavFunction(screenSize)
 
 function navHighlighter() {
   
@@ -23,4 +26,38 @@ function navHighlighter() {
       secClass.remove("active");
     }
   });
+}
+
+function navChanger(){
+  let scrollY = window.pageYOffset;
+  
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop -64; // - sign for color change to happen earlier. + sign for later
+    let sectionId = current.getAttribute("id");
+
+    //let navBarIndicator = document.querySelector(".mobile-nav p"); //code motion optimization
+    let navBarIndicator = document.getElementById("nav-section")
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      let section = sectionId.split("-")[0]
+      if(section === "home"){
+        navBarIndicator.textContent = "Welcome"
+      }
+      else if(section === "about"){
+        navBarIndicator.textContent = "About Me"
+      } 
+      else{
+        navBarIndicator.textContent = "My Socials"
+      }
+    } 
+  });
+}
+
+function updateNavFunction(media){
+  if(media.matches){
+    window.addEventListener("scroll", navHighlighter);
+  }
+  else{
+    window.addEventListener("scroll", navChanger)
+  }
 }
